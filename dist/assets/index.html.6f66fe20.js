eb8bb2c972d9ec5550ca6691e3863714b4cdaac6
kinronzhang <kinronzhang@tencent.com>
2022-07-09 13:59:07 +0800

    feat: update

---
+++ dist/assets/index.html.6f66fe20.js
@@ -0,0 +1,1 @@
+const e=JSON.parse('{"key":"v-8daa1a0e","path":"/","title":"Hello VuePress","lang":"zh-CN","frontmatter":{},"excerpt":"","headers":[],"git":{"updatedTime":1657343348000,"contributors":[{"name":"kinronzhang","email":"kinronzhang@tencent.com","commits":1}]},"filePathRelative":"README.md"}');export{e as data};
--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,4 +2,5 @@ module.exports = {
   lang: 'zh-CN',
   title: '风时雨',
   description: 'VuePress 站点',
+  dest: 'dist'
 }
