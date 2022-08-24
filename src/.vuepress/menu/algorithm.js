23ec0cf132b3a1392e58547be3593edcb601abd1
zjl-coder <1301034106@qq.com>
2022-08-24 23:48:45 +0800

    feat: update

--- src/.vuepress/menu/algorithm.js
+++ src/.vuepress/menu/algorithm.js
@@ -5,6 +5,20 @@ const algorithm = {
     {
       text: '排序算法',
       link: '/algorithm/sort.md',
+    },
+    {
+      text: '高级算法',
+      link: '/algorithm/leetcode/hard',
+      children: [
+        {
+          text: '盛最多水的容器',
+          link: '/algorithm/leetcode/hard/2.md',
+        },
+        {
+          text: '缺失的第一个正数',
+          link: '/algorithm/leetcode/hard/1.md',
+        }
+      ],
     }
   ],
 };
---
+++ src/algorithm/leetcode/hard/index.md
@@ -0,0 +1,2 @@
+# leetcode 高级算法
+[高级算法](https://leetcode.cn/leetbook/read/top-interview-questions-hard/xqjfxh/)
