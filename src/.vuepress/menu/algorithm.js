761c4d1fff976a641699b727dd7a98eae3ad6686
zjl-coder <1301034106@qq.com>
2022-08-26 00:04:13 +0800

    feat: update

--- src/.vuepress/menu/algorithm.js
+++ src/.vuepress/menu/algorithm.js
@@ -11,13 +11,33 @@ const algorithm = {
       link: '/algorithm/leetcode/hard',
       children: [
         {
+          text: '除自身以外数组的乘积',
+          link: '/algorithm/leetcode/hard/4.md',
+        },
+        {
+          text: '螺旋矩阵',
+          link: '/algorithm/leetcode/hard/5.md',
+        },
+        {
+          text: '四数相加 II',
+          link: '/algorithm/leetcode/hard/6.md',
+        },
+        {
           text: '盛最多水的容器',
           link: '/algorithm/leetcode/hard/2.md',
         },
         {
+          text: '生命游戏',
+          link: '/algorithm/leetcode/hard/7.md',
+        },
+        {
           text: '缺失的第一个正数',
           link: '/algorithm/leetcode/hard/1.md',
-        }
+        },
+        {
+          text: '接雨水',
+          link: '/algorithm/leetcode/hard/3.md',
+        },
       ],
     }
   ],
