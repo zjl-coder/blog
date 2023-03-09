c168bcb7a1452dd7de9c995297511a11e32af482
zjl-coder <1301034106@qq.com>
2023-03-09 10:37:32 +0800

    feat: update

--- push.sh
+++ push.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
+
+git config user.name 'zjl-coder'
+git config user.email '1301034106@qq.com'
+
 npx prettier --write ./src/.vuepress/components/* ./src/.vuepress/menu/* ./src/.vuepress/*.js
 
 git add .
